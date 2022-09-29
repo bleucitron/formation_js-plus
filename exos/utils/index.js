1da4a09689d7f50eb2465c82660a2a996f3d2b36
Romain Crestey <rcrestey@gmail.com>
2022-09-29 11:15:53 +0200

    fix utils links

---
+++ exos/utils/index.js
@@ -0,0 +1,25 @@
+export function createRandomNumber() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      const n = Math.floor(Math.random() * 100);
+
+      resolve(n);
+    }, 3000);
+  });
+}
+
+export function waitACertainTime() {
+  const maxTime = 3500;
+
+  return new Promise(function (resolve, reject) {
+    const delay = Math.floor(Math.random() * 7000);
+
+    setTimeout(() => {
+      resolve(delay);
+    }, delay);
+
+    setTimeout(() => {
+      reject('Trop long...');
+    }, maxTime);
+  });
+}
